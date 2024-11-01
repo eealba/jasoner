3bb1a4eb7d475e583f4fcdb92bcbadb772c7bbaf
Edgar Enrique Alba Barrile <eealba@gmail.com>
2024-11-01 21:41:29 +0100

    SerializationStrategy BOTH, METHOD and FIELD

--- src/main/java/io/github/eealba/jasoner/internal/JsonSerializerImpl.java
+++ src/main/java/io/github/eealba/jasoner/internal/JsonSerializerImpl.java
@@ -22,7 +22,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 
 /**
  * The class JsonSerializerImpl.
